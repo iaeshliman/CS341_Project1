189bf076ee7bf2abf1ff005333fb780bd388cc0d
RHaffeman <ryanhaffeman@gmail.com>
2019-09-23 11:12:34 -0500

    Finished addItem in datamanger
    as title, creates id as empty string plus random int from 0-999998. very low probability of duplicate numbers, grows with number of uses though.

--- CS341_Project1_Copy/src/DataManager.java
+++ CS341_Project1_Copy/src/DataManager.java
@@ -1,14 +1,22 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 
 public class DataManager {
 	File file=new File("Data.txt");
 	
-	public void addItem(String id, String name, double price, int availability) throws IOException{
+	MultiList mList;
+	Random rng=new Random();
+	String id;
+	
+	public void addItem(String name, double price, int availability) throws IOException{
+		id=""+rng.nextInt(999999);
 		String fileInputString="\n"+id+"\t \t"+name+"\t \t"+price+"\t \t"+availability;
 		FileWriter write=new FileWriter("Data.txt");
 		write.write(fileInputString);
+		
+		mList.addItem(id, name, price, availability);
 		
 	}
 }
